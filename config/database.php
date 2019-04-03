1ad0bfcb64eea6d15ef37d7dc3b535c45bb0e2b3
Murray Crane <murray.crane@ggpsystems.co.uk>
2019-04-03 10:42:21 +0100

    Configuration changes to allow for multi-database usage.

--- config/database.php
+++ config/database.php
@@ -85,6 +85,17 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'sqlsrv2' => [
+            'driver' => 'sqlsrv',
+            'host' => env('DB_HOST_SECOND', 'localhost'),
+            'port' => env('DB_PORT_SECOND', '1433'),
+            'database' => env('DB_DATABASE_SECOND', 'forge'),
+            'username' => env('DB_USERNAME_SECOND', 'forge'),
+            'password' => env('DB_PASSWORD_SECOND', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+        ],
     ],
 
     /*
