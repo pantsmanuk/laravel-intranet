ebc43618aa49e01e04084b864d410fde33983bd8
Murray Crane <murray.crane@ggpsystems.co.uk>
2019-08-14 17:19:40 +0100

    Added Forenames accessor to User model

--- app/User.php
+++ app/User.php
@@ -13,30 +13,22 @@ class User extends Authenticatable implements Auditable
     use SoftDeletes;
     use \OwenIt\Auditing\Auditable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'username', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getForenamesAttribute()
+    {
+        $forenames = strtok($this->name, ' ');
+
+        return ucfirst($forenames);
+    }
 }
