1f7430af494110127ccfbb14b16329056e3bb258
Murray Crane <murray.crane@ggpsystems.co.uk>
2019-08-15 16:20:51 +0100

    Extended MySqlGrammer class to permit creation of BINARY type fields.

---
+++ app/MySqlGrammar.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App;
+
+use Illuminate\Support\Fluent;
+
+
+class MySqlGrammar extends \Illuminate\Database\Schema\Grammars\MySqlGrammar
+{
+    protected function typeRealBinary(Fluent $column)
+    {
+        return "binary({$column->length})";
+    }
+}
