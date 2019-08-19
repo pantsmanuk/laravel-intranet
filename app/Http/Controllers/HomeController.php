c51d96da99a9f1def82caace8bfbbd7640c82969
Murray Crane <murray.crane@ggpsystems.co.uk>
2019-08-19 13:28:03 +0100

    Fixed todo - Time & Attendance for Remote Workers. First pass, this allows remote staff to indicate attendance.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Absence;
+use App\DoorEvent;
 use App\Telephone;
+use App\User;
 use App\WorkState;
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Date;
 
 class HomeController extends Controller
@@ -48,16 +50,16 @@ class HomeController extends Controller
                 ->where('telephones.name', '!=', 'Extn')
                 ->orderBy('telephones.name')
                 ->get();
-            $absence = Absence::select('absence_types.name AS workstate')
+            $absence = Absence::select('absence_types.name AS work_state')
                 ->join('absence_types', 'absences.absence_id', '=', 'absence_types.id')
                 ->where('absences.user_id', $employee->id)
                 ->where('absences.started_at', '<=', $dt->toDateTimeString())
                 ->where('absences.ended_at', '>=', $dt->toDateTimeString())
                 ->first();
             if (!is_null($absence)) {
-                $employee['workstate'] = $absence->workstate;
+                $employee['work_state'] = $absence->work_state;
             } else {
-                $employee['workstate'] = WorkState::where('id', $employee->default_workstate_id)
+                $employee['work_state'] = WorkState::where('id', $employee->default_work_state_id)
                     ->pluck('work_state')
                     ->first();
             }
@@ -65,6 +67,41 @@ class HomeController extends Controller
             return $employee;
         });
 
-        return view('home')->with('staff', $staff);
+        $remotes = $staff->filter(function ($employee) {
+            if ($employee['work_state'] === 'Remote working') {
+                $employee['door_event'] = DoorEvent::where('user_id', '=', $employee->id)
+                    ->whereDate('created_at', Date::now('Europe/London'))
+                    ->latest('created_at')
+                    ->pluck('event')
+                    ->first();
+                if (is_null($employee['door_event'])) {
+                    $employee['door_event'] = 1; // First touch should be an In
+                }
+
+                return $employee;
+            }
+        });
+
+        return view('home')->with([
+            'remotes' => $remotes,
+            'staff' => $staff,
+        ]);
+    }
+
+    /**
+     * Store a T&A record for remote workers.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData['user_id'] = auth()->id();
+        $validatedData['event'] = (isset($request['event'])) ? 1 : 0;
+
+        DoorEvent::create($validatedData);
+
+        return redirect('/home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
     <div class="container">
+        <?php $user = $remotes->where('id', auth()->id())->first();
+        if (isset($user)) { ?>
+        <div class="row justify-content-center d-print-none">
+            <div class="embed-responsive">
+                <h4 class="float-left">My Attendance</h4>
+                <form class="float-right" method="post" action="{{route('home.store')}}">
+                    <div class="form-group">
+                        @csrf
+                        <label for="event">I&#39;m currently </label>
+                        <input id="event" name="event" type="checkbox" data-toggle="toggle" data-on="Out"
+                               data-off="In" data-onstyle="danger" data-offstyle="success" onchange="this.form.submit()"
+                               <?php echo ($user->door_event == 1) ? 'checked': '';?>/>
+                    </div>
+                </form>
+                <span class="float-none">&nbsp;</span>
+                <hr>
+            </div>
+        </div>
+        <?php } ?>
         <div class="row justify-content-center">
             <div class="table-responsive">
                 <h4 class="float-left">Telephone Directory</h4><h4 class="float-right">Switchboard: 020 8686 9887</h4>
@@ -19,7 +38,7 @@
                     <tbody>
                     @foreach ($staff as $value)
                         <tr>
-                            <td>{{$value->name}} <em>{{$value->workstate}}</em></td>
+                            <td>{{$value->name}} <em>{{$value->work_state}}</em></td>
                             <td>{{$value->extn}}</td>
                             <td>@foreach ($value->telephones as $telephone){{$telephone->name}}: {{$telephone->number}}
                                 <br/>@endforeach</td>
@@ -30,4 +49,7 @@
             </div>
         </div>
     </div>
+    <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.5.0/css/bootstrap4-toggle.min.css"
+          rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.5.0/js/bootstrap4-toggle.min.js"></script>
 @endsection
